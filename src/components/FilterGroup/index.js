9c425c25ef56994be22e984d039583df493678db
Sreeja <sreejameesala123@gmail.com>
2025-02-27 16:41:52 +0000

    Completed Emplyee and Salary Filteration

--- src/components/FilterGroup/index.js
+++ src/components/FilterGroup/index.js
@@ -1,32 +1,59 @@
 import {Component} from 'react'
 
+import Cookies from 'js-cookie'
+
 import './index.css'
 
 class FilterGroup extends Component {
-  state = {employee: []}
+  state = {employee: [], salary: ''}
 
   componentDidMount() {
     this.getJobsApICall()
   }
 
-  getJobsApICall = () => {
-    // const jobsApiUrl = 'https://apis.ccbp.in/jobs'
+  getJobsApICall = async () => {
+    const {employee, salary} = this.state
+    let employeeTypeValue
+    if (employee.length === 0) {
+      employeeTypeValue = ''
+    } else {
+      employeeTypeValue = employee.join(',')
+    }
+    const jobsApiUrl = `https://apis.ccbp.in/jobs?employment_type=${employeeTypeValue}&minimum_package=${salary}&search=`
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(jobsApiUrl, options)
+    console.log(response)
   }
 
   employementChange = event => {
     const {employee} = this.state
-    const value = event.target
+    const elementValue = event.target.value
     if (event.target.checked) {
-      const element = employee.includes(value)
+      const element = employee.includes(elementValue)
       if (!element) {
-        this.setState(prevState => ({
-          employee: [...prevState.employee, event.target.value],
-        }))
+        this.setState(
+          prevState => ({
+            employee: [...prevState.employee, elementValue],
+          }),
+          this.getJobsApICall,
+        )
       }
     } else {
-      const newEmployeeList = employee.filter(eachType => eachType !== value)
-      this.setState({employee: newEmployeeList})
+      const newEmployeeList = employee.filter(
+        eachType => eachType !== elementValue,
+      )
+      this.setState({employee: newEmployeeList}, this.getJobsApICall)
     }
+  }
+
+  salaryRangeChanges = event => {
+    this.setState({salary: event.target.value}, this.getJobsApICall)
   }
 
   employeeTypes = () => {
@@ -62,6 +89,7 @@ class FilterGroup extends Component {
           {salaryRangesList.map(eachType => (
             <li>
               <input
+                onChange={this.salaryRangeChanges}
                 type="radio"
                 id={eachType.salaryRangeId}
                 name="jobType"
@@ -78,12 +106,9 @@ class FilterGroup extends Component {
   }
 
   render() {
-    // const {employee} = this.state
-    // console.log(employee)
+    const {employee} = this.state
+    console.log(employee)
     // const {employmentTypesList, salaryRangesList} = this.props
-    // console.log(employmentTypesList)
-    // console.log(salaryRangesList)
-
     return (
       <div className="FilterGroupContainer">
         {this.employeeTypes()}
